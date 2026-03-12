2b3295af89d4e645b45c18599405ed654853c6a9
Rishov-NightKING <rishovpaul97@gmail.com>
2026-03-12 03:31:07 -0400

    fixed

--- train_both_contour_motion.py
+++ train_both_contour_motion.py
@@ -22,19 +22,19 @@ diffusion = GaussianDiffusion(
     contour_noise_only = False,  # True = noise only in mask contour region, False = noise on full image
 ).cuda()
 
-exp_name = "experiment-name"
+exp_name = "noise-contour-region-cond-mask-motion-both"
 
 
 trainer = Trainer(
     diffusion_model=diffusion,
     input_video_folder=f'{base_path}/dense/train/displacement_dense',
     contour_condition_video_dir=f'{base_path}/dense/train/dense_mask',
-    motion_condition_video_dir=f'{base_path}/dense/train/motion_dense',
+    motion_condition_video_dir=f'{base_path}/tlrn_dense_mask_motion/train',
     sampling_contour_condition_video_dir=f'{base_path}/dense/test/dense_mask',
     sampling_motion_condition_video_dir=f'{base_path}/tlrn_dense_mask_motion/test',
     train_batch_size = 20,
     train_lr = 1e-5,
-    save_and_sample_every = 5,
+    save_and_sample_every = 500,
     train_num_steps = 700000,         # total training steps
     gradient_accumulate_every = 1,    # gradient accumulation steps
     ema_decay = 0.995,                # exponential moving average decay
